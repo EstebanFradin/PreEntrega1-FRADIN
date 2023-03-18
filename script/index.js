ce02edba9817baaefa789dfef587cd736e3f431e
Esteban Fradin <estefradin@gmail.com>
2023-03-18 02:55:08 -0300

    Se agregó API e intento de finalizar carrito

--- script/index.js
+++ script/index.js
@@ -1,123 +1,169 @@
 
 // ********************************** VARIABLES ************************************** //
 
-let cart = [];
-let total = 0;
-const cartTable = document.querySelector('#cart tbody');
-const totalCell = document.querySelector('#total');
-const checkoutBtn = document.querySelector('#checkout');
+const arrayVacio = [];
 
-// ********************************** FUNCIONES ************************************** //
 
-function addToCart(producto, cantidad, precio) {
-    const item = {
-      producto,
-      cantidad,
-      precio,
-      total: cantidad * precio,
-    };
-    
-    cart.push(item);
-    
-    updateCartTable();
-    saveCart();
+// ********************************** API ************************************** //
+const lista = document.querySelector("#listado");
+
+
+class Productos {
+  constructor(products) {
+    this.id = products.id;
+    this.nombre = products.nombre;
+    this.precio = products.precio;
+    this.img = products.img;
+    this.cantidad = products.cantidad;
+    this.precioTotal = products.precio;
   }
 
-  function updateCartTable() {
-    const table = document.querySelector('#cart tbody');
-    table.innerHTML = '';
-    
-    cart.forEach((item, index) => {
-      const row = table.insertRow();
-      
-      const productoCell = row.insertCell();
-      productoCell.textContent = item.producto;
-      
-      const cantidadCell = row.insertCell();
-      cantidadCell.textContent = item.cantidad;
-      
-      const precioCell = row.insertCell();
-      precioCell.textContent = item.precio;
-      
-      const totalCell = row.insertCell();
-      totalCell.textContent = item.total;
-      
-      const removeBtn = document.createElement('button');
-      removeBtn.textContent = 'Eliminar';
-      removeBtn.classList.add('btn', 'btn-dark');
-      
-      removeBtn.addEventListener('click', () => {
-        removeItem(index);
+  agregarUnidad() {
+    this.cantidad++;
+  }
+
+  quitarUnidad() {
+    this.cantidad--;
+  }
+
+  actualizarPrecioTotal() {
+    this.precioTotal = this.precio * this.cantidad;
+  }
+}
+
+function imprimirProductosEnHTML() {
+
+    let contenedor = document.getElementById("contenedor");
+    contenedor.innerHTML = "";
+
+    fetch('https://dummyjson.com/products')
+      .then((data) => {
+        return data.json();
+      })
+      .then((res) => {
+        res.products.forEach((el) => {
+          lista.innerHTML += `
+              <div class="cajas">
+                <div class="center-img-api">
+                  <img src="${el.images[0]}" class="images-api">
+                </div>
+                <hr>
+                <h4 class="center-text-api">${el.title}</h4>
+                <p class="center-text-api">${el.price}$</p>
+                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                    <button id="agregar${el.title}${el.id}" type="button" class="btn btn-dark"> Agregar </button>
+                </div>
+              </div>`;
+        });
       });
-      
-      const removeCell = row.insertCell();
-      removeCell.appendChild(removeBtn);
-    });
-    
-    const total = cart.reduce((acc, item) => acc + item.total, 0);
-    document.querySelector('#cart tfoot td:last-child').textContent = total;
+
+    contenedor.appendChild(lista);
+    let boton = document.getElementById(`agregar${produc.nombre}${produc.id}`);
+    boton.addEventListener("click", () => agregarAlCarrito());
   }
+  
+  
 
-  function updateTotal() {
-    total = cart.reduce((acc, item) => acc + item.total, 0);
-    totalCell.textContent = total;
-  }
+function agregarAlCarrito(producto) {
 
+  let index = carrito.findIndex((elemento) => elemento.id === producto.id);
+  console.log({ index });
 
-  const productos = [
-    { nombre: 'Camisa', precio: 500 },
-    { nombre: 'Pantalón', precio: 800 },
-    { nombre: 'Zapatos', precio: 1200 },
-  ];
+  if (index != -1) {
+    carrito[index].agregarUnidad();
+    carrito[index].actualizarPrecioTotal();
+  } else {
 
-  function mostrarProductos() {
-    const productosList = document.querySelector('#productos');
-    
-    productos.forEach(producto => {
-      const item = document.createElement('li');
-      item.textContent = `${producto.nombre}: $${producto.precio}`;
-      
-      const addBtn = document.createElement('button');
-      addBtn.textContent = 'Agregar al carrito';
-      addBtn.classList.add('btn', 'btn-dark');
-      
-      addBtn.addEventListener('click', () => {
-        addToCart(producto.nombre, 1, producto.precio);
-      });
-      
-      item.appendChild(addBtn);
-      
-      productosList.appendChild(item);
-    });
-  }
-
-  function saveCart() {
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
-
-  function loadCart() {
-    const savedCart = localStorage.getItem('cart');
-    
-    if (savedCart) {
-      cart = JSON.parse(savedCart);
-      updateCartTable();
-    }
-  }
-
-  window.addEventListener('load', () => {
-    loadCart();
-  });
-
-  function removeItem(index) {
-    cart.splice(index, 1);
-    updateCartTable();
-    saveCart();
+    let prod = new Productos(producto);
+    prod.cantidad = 1;
+    carrito.push(prod);
   }
 
 
-  // ********************************** LLAMADO DE FUNCION ************************************** //
-  mostrarProductos();
+  localStorage.setItem("carritoEnStorage", JSON.stringify(carrito));
+  imprimirTabla(carrito);
+}
+
+function eliminarDelCarrito(id) {
+
+  let index = carrito.findIndex((element) => element.id === id);
+  
+  if (carrito[index].cantidad > 1) {
+      carrito[index].quitarUnidad();
+      carrito[index].actualizarPrecioTotal();
+  } else {
+
+      carrito.splice(index, 1);
+  }
 
 
+  localStorage.setItem("carritoEnStorage", JSON.stringify(carrito));
+  imprimirTabla(carrito);
+}
 
+function eliminarCarrito() {
+  carrito = [];
+  localStorage.removeItem("carritoEnStorage");
 
+  swal("Compra eliminada con éxito", "", "success");
+
+  document.getElementById("tabla-carrito").innerHTML = "";
+  document.getElementById("acciones-carrito").innerHTML = "";
+}
+
+function obtenerPrecioTotal(array) {
+  return array.reduce((total, elemento) => total + elemento.precioTotal, 0);
+}
+
+function imprimirTabla(array) {
+  let contenedor = document.getElementById("tabla-carrito");
+  contenedor.innerHTML = "";
+
+  let tabla = document.createElement("div");
+
+  tabla.innerHTML = `
+      <table id="tablaCarrito" class="table table-striped">
+          <thead>         
+              <tr>
+                  <th>Item</th>
+                  <th>Cantidad</th>
+                  <th>Precio</th>
+                  <th>Total</th>
+              </tr>
+          </thead>
+
+          <tbody id="bodyTabla">
+
+          </tbody>
+      </table>
+  `;
+
+  contenedor.appendChild(tabla);
+
+  let bodyTabla = document.getElementById("bodyTabla");
+
+  for (let produc of array) {
+      let datos = document.createElement("tr");
+      datos.innerHTML = `
+              <td>${produc.title}</td>
+              <td>${produc.precio}</td>
+              <td>$${alfajor.precioTotal}</td>
+              <td><button id="eliminar${produc.id}" class="btn btn-dark">Eliminar</button></td>
+    `;
+
+      bodyTabla.appendChild(datos);
+
+      let botonEliminar = document.getElementById(`eliminar${produc.id}`);
+      botonEliminar.addEventListener("click", () => eliminarDelCarrito(produc.id));
+  }
+
+  let precioTotal = obtenerPrecioTotal(array);
+  let accionesCarrito = document.getElementById("acciones-carrito");
+  accionesCarrito.innerHTML = `
+  <h5>PrecioTotal: $${precioTotal}</h5></br>
+  <button id="vaciarCarrito" onclick="eliminarCarrito()" class="btn btn-dark">Vaciar Carrito</button>
+`;
+}
+
+imprimirProductosEnHTML();
+let carrito = chequearCarritoEnStorage();
